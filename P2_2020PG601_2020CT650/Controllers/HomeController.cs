018528e2580a294b068326cb30bbccd021e9a4be
melan <melan@LAPTOP-30ROFJGU>
2023-04-29 12:13:56 -0600

    Agregar archivos de proyecto.

---
+++ P2_2020PG601_2020CT650/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P2_2020PG601_2020CT650.Models;
+using System.Diagnostics;
+
+namespace P2_2020PG601_2020CT650.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ P2_2020PG601_2020CT650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2020PG601_2020CT650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
